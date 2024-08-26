8ad059bf060a626a1d2bfdf69f5d4538d025b915
kili196 <kilian.baier16@gmail.com>
2024-08-26 16:40:32 +0200

    modifed loadDom.js

--- src/components/loadDom.js
+++ src/components/loadDom.js
@@ -2,7 +2,8 @@ function loadDomElements() {
     const projects = document.getElementsByClassName("project");
     const project_name = document.getElementsByClassName("project-name");
     const project_list = document.getElementsByClassName("project-list");
-    return ({ "projects": projects, "project_name": project_name, "project_list": project_list });
+    const project_todos = document.getElementsByClassName("project-todos")
+    return ({ "projects": projects, "project_name": project_name, "project_list": project_list, "project_todos": project_todos });
 }
 
 export { loadDomElements }
