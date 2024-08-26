5d6ee76038474171727549a99355d10d5cc4f867
kili196 <kilian.baier16@gmail.com>
2024-08-26 20:06:22 +0200

    fixed bug in showing diffrent tasks of the projectrs

--- src/index.js
+++ src/index.js
@@ -15,15 +15,12 @@ const loadedDom = loadDomElements();
 generateProjectAndAddItToDom("Project-1")
 generateProjectAndAddItToDom("Project-2")
 generateProjectAndAddItToDom("Project-3")
-generateProjectAndAddItToDom("Project-4")
+generateProjectAndAddItToDom("Project-4").addTaskToProject("hiii")
 
 
 Array.from(loadedDom.projects).forEach(project => project.addEventListener("click", () => {
     loadedDom.project_name[0].innerHTML = project.innerHTML;
-    console.log(projects[project.id].projectTasks.push("hii"));
-    console.log(projects[project.id].projectTasks)
-    console.log(project)
-    console.log(projects[project.id].projectTasks)
+
     loadTasks(projects[project.id])
 
 }));
