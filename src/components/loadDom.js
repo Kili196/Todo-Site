4a12a673beeccb39caa25b6bed4b30559b78f6e9
kili196 <kilian.baier16@gmail.com>
2024-08-29 14:02:50 +0200

    updated date class

--- src/components/loadDom.js
+++ src/components/loadDom.js
@@ -3,7 +3,7 @@ function loadDomElements() {
     const project_name = document.getElementsByClassName("project-name");
     const project_list = document.getElementsByClassName("project-list");
     const project_todos = document.getElementsByClassName("project-todos");
-    const date_div = document.getElementsByClassName("date_div");
+    const date_div = document.getElementsByClassName("date");
     return ({
         "projects": projects, "project_name": project_name,
         "project_list": project_list, "project_todos": project_todos, "date_div": date_div
