964ea1c5d46b1159214285e80db7990b911bc3d8
kili196 <kilian.baier16@gmail.com>
2024-09-03 14:52:19 +0200

    added modal and updated loadDomElemets

--- src/components/loadDom.js
+++ src/components/loadDom.js
@@ -7,9 +7,10 @@ function loadDomElements() {
     const addTaskButton = document.getElementsByClassName("add-task-button");
     const todo_view = document.getElementsByClassName("todo-view");
     const noProject_screen = document.getElementsByClassName("no-project-screen");
+    const addTaskModal = document.getElementsByClassName("add-task-modal");
     return ({
         "projects": projects, "project_name": project_name,
-        "project_list": project_list, "project_todos": project_todos, "date_div": date_div, addTaskButton, todo_view: todo_view, noProject_screen
+        "project_list": project_list, "project_todos": project_todos, "date_div": date_div, addTaskButton, todo_view: todo_view, noProject_screen, addTaskModal
     });
 }
 
