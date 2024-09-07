738ffba08c77405028e950648d44a66b82b3c1d3
kili196 <kilian.baier16@gmail.com>
2024-09-07 15:13:37 +0200

    added id to project and to task

--- src/components/generateProjectObject.js
+++ src/components/generateProjectObject.js
@@ -2,13 +2,16 @@ import { createTodo } from "./generateTodo"
 
 function createProject(projectName) {
     const projectTasks = [];
+    let id = 0;
 
     return {
         "projectName": projectName,
         "projectTasks": projectTasks,
         addTaskToProject(taskName) {
             projectTasks.push(createTodo(taskName));
-        }
+            id++;
+        },
+        "id": id,
     }
 }
 
