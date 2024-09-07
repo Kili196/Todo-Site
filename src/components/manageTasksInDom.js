8ea1b4ef164003dacdf24c934c051df6eae9e4bd
kili196 <kilian.baier16@gmail.com>
2024-09-07 15:57:50 +0200

    showing no task screen working and code refactoring

--- src/components/manageTasksInDom.js
+++ src/components/manageTasksInDom.js
@@ -8,58 +8,84 @@ function deleteTaskFromDom(todo_div, checkBoxToDelete, project) {
     project.projectTasks.splice(project.projectTasks.findIndex(task => task.id === taskToFind), 1);
 }
 
+function showNoTaskScreen(projectTodos) {
+    const noTodoDiv = document.createElement("div");
+    noTodoDiv.innerHTML = "NO TASKS IN THIS PROJECT, CLICK ABOVE TO ADD SOME";
+    noTodoDiv.className = "noTodoScreen"
+    projectTodos.appendChild(noTodoDiv);
+}
+
+function generateCheckBox(task) {
+    const checkBoxToDelete = document.createElement("input");
+    checkBoxToDelete.setAttribute("type", "checkbox");
+    checkBoxToDelete.className = "checkBoxToDeleteTask";
+    checkBoxToDelete.id = task.id;
+
+    return checkBoxToDelete;
+}
+
+function generateTodoContainer(task) {
+    const todoContainer = document.createElement("div");
+    const todoheadline_div = document.createElement("div");
+
+    todoheadline_div.className = "todoheadline"
+    todoheadline_div.innerHTML = task.todoName;
+
+    const creation_date = document.createElement("div");
+    creation_date.className = "creation-date"
+    creation_date.innerHTML = "created at " + task.creationDate;
+
+    todoContainer.appendChild(todoheadline_div);
+    todoContainer.appendChild(creation_date)
+
+    return todoContainer;
+}
+
 function loadTasks(project) {
 
     const projectTodos = loadDomElements().project_todos[0];
 
 
 
+
     document.getElementsByClassName("project-todos")[0].innerHTML = ""
+    if (project.projectTasks.length != 0) {
 
-    project.projectTasks.forEach(task => {
-
-
-        const todo_div = document.createElement("div");
-        todo_div.className = "todo";
-
-        const divForCheckBox = document.createElement("div");
-        divForCheckBox.className = "checkBoxDiv";
+        project.projectTasks.forEach(task => {
 
 
-        const checkBoxToDelete = document.createElement("input");
-        checkBoxToDelete.setAttribute("type", "checkbox");
-        checkBoxToDelete.className = "checkBoxToDeleteTask";
-        checkBoxToDelete.id = task.id;
+            const todo_div = document.createElement("div");
+            todo_div.className = "todo";
 
-        checkBoxToDelete.addEventListener("click", () => {
-            deleteTaskFromDom(todo_div, checkBoxToDelete, project);
-        })
+            const divForCheckBox = document.createElement("div");
+            divForCheckBox.className = "checkBoxDiv";
 
 
+            const checkBoxToDelete = generateCheckBox(task);
 
-        divForCheckBox.appendChild(checkBoxToDelete);
-        todo_div.appendChild(divForCheckBox);
-
-        const todoContainer = document.createElement("div");
-        const todoheadline_div = document.createElement("div");
-
-        todoheadline_div.className = "todoheadline"
-        todoheadline_div.innerHTML = task.todoName;
-
-        const creation_date = document.createElement("div");
-        creation_date.className = "creation-date"
-        creation_date.innerHTML = "created at " + task.creationDate;
-
-        todoContainer.appendChild(todoheadline_div);
-        todoContainer.appendChild(creation_date)
-
-        todo_div.appendChild(todoContainer);
-        projectTodos.appendChild(todo_div);
+            checkBoxToDelete.addEventListener("click", () => {
+                deleteTaskFromDom(todo_div, checkBoxToDelete, project);
+                if (project.projectTasks.length == 0) {
+                    showNoTaskScreen(projectTodos)
+                }
+            })
 
 
 
+            divForCheckBox.appendChild(checkBoxToDelete);
+            todo_div.appendChild(divForCheckBox);
 
-    });
+            const todoContainer = generateTodoContainer(task);
+
+            todo_div.appendChild(todoContainer);
+            projectTodos.appendChild(todo_div);
+        });
+    }
+    else {
+        showNoTaskScreen(projectTodos);
+
+
+    }
 }
 
 export { loadTasks }
