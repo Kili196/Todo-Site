f473532e1fedf90f92b6bef733016b9ce82b592e
kili196 <kilian.baier16@gmail.com>
2024-09-09 16:46:22 +0200

    added project with currentTasks to dom

--- src/components/generateProjectObject.js
+++ src/components/generateProjectObject.js
@@ -3,14 +3,14 @@ import { createTodo } from "./generateTodo"
 function createProject(projectName) {
     const projectTasks = [];
     let id = 0;
-
+    console.log(localStorage.getItem("Project-4"));
     return {
         "projectName": projectName,
         "projectTasks": projectTasks,
         addTaskToProject(taskName) {
             const createdTask = createTodo(taskName, id);
             projectTasks.push(createdTask);
-            localStorage.setItem(projectName, projectTasks);
+            localStorage.setItem(projectName, JSON.stringify(projectTasks));
             id++;
         },
 
